c9f26d1bfbd3c87723fc60d71325eaf51b260492
js-nowloading <js-nowloading@protonmail.com>
2022-07-01 13:21:20 +0800

    first commit

---
+++ content.js
@@ -0,0 +1,31 @@
+const text =
+`*** JS-KIZUNA-SYSTEM ver.1.2.4 21123 (Aug 15 2012 - 22:14:21)
+ 
+*** Checking ***verse:
+dWorld-EICHI         ########[OK]
+dWorld-SHINPI        ########[OK]
+dWorld-YUUAI         ########[OK]
+ 
+*** no sdio debug board detected
+*** EEEE I3000000032940xf100110303B77500EEEE I400000004294_M8_BL1_10221D113B323EEEE5
+ 
+*** Restoring volume: 0x55090C7C768166F6DB423C660c333B5EF035E2b6
+*** Restoring volume: 0xF67c59F74143D3bd632bB0d0C42B021304dFB33c
+*** Restoring volume: 0x8ae8a53147eB11618A1AA0f193c107Ac40eb6f8B
+*** Restoring volume: 0xdAF97b923303820C4476c2943647bC720Ec68D43
+ 
+Booting........
+*** Failed. ERROR: 22421312495966 (Unknown)
+ 
+Booting........
+*** Partially Succeeded.
+ 
+K.I.Z.U.N.A version 1.2.4 21123
+ 
+*** Mounting volume: 0x55090C7C768166F6DB423C660c333B5EF035E2b6 [OK]
+*** Mounting volume: 0xF67c59F74143D3bd632bB0d0C42B021304dFB33c [OK]
+*** Mounting volume: 0x8ae8a53147eB11618A1AA0f193c107Ac40eb6f8B [OK]
+*** Mounting volume: 0xdAF97b923303820C4476c2943647bC720Ec68D43 [OK]
+ 
+Connecting to ***verse.............
+*** [Press Enter to Continue]`
